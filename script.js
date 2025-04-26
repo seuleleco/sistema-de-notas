edd54ebf81e2e2a13041d0ef57d2fb8e188e1080
ILHA SERVICE - Leonardo Pereira Pereira <leonardo.pereira.3@ebserh.gov.br>
2025-04-26 18:17:35 -0300

    inicio de melhorias

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const gradients = [
+    {
+    background:'linear-gradient(to right, rgb(255, 255, 255), rgb(51, 92, 115))',
+    color: "#000000"
+    },
+
+    {
+    background:'linear-gradient(to right, rgb(109, 109, 109),  rgb(0, 0, 0))',
+    color: "#000000"
+    }
+    
+
+];
+
+let currentIndex = 0;
+
+function trocartema(){
+  currentIndex = (currentIndex + 1) % gradients.length;
+    const cxt = document.getElementById("caixatema")
+    cxt.style.background = gradients[currentIndex].background;
+    cxt.style.color = gradients[currentIndex].color;
+    
+    const temastatus = document.getElementById("temastatus");
+
+    temastatus.classList.add("fade-out");
+
+    setTimeout(() => {
+        if (currentIndex === 0) {
+        document.getElementById("temastatus").src = "images/sol.png";
+        
+    } else {
+        document.getElementById("temastatus").src = "images/lua.png";
+        
+    }
+    temastatus.classList.remove("fade-out");
+}, 500);
+
+}
+
 
 let alunos = JSON.parse(localStorage.getItem("alunos")) || [];
 
